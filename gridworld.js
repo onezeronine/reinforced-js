fcc88548ff438d398390af9a08d2a2f02bf6855d
onezeronine <bastian.kenneth.g@gmail.com>
2014-11-08 21:33:46 +0800

    fix bug on getMax method

--- gridworld.js
+++ gridworld.js
@@ -1,143 +1,16 @@
 (function() {
-    //Q-learning algorithm
-    //Initialize Q(s,a) arbitrarily
-    var dimension = 3;
-    var blocks = [];
-    var episodes = 50;
-    var steps = 10;
-    var discount = 0.401;
-    var theta = 0.24;
-    var actionList = document.querySelector('#actions');
     var canvas = document.querySelector("#canvas");
     var context = canvas.getContext('2d');
-    var size = canvas.width / dimension;
+    var env = new Environment(3, 0.41, 0.34);
     
-    for(var i = 0; i < dimension; ++i)
-    {
-        blocks.push([]);
-        for(var j = 0; j < dimension; j++)
-        {
-            blocks[i][j] = {
-                actions: [randomInterval(0.45,0.66), randomInterval(0.35,0.7), randomInterval(0.4,0.58), randomInterval(0.35,0.7)],
-                reward: 0
-            };
-        }
-    }
+    var go = document.querySelector("#go");
+    env.draw(canvas,context);
+    go.onclick = function()
+    {    
+        env.draw(canvas,context);
+        env.step(1);
+    };
     
-    //set reward
-    blocks[2][2].reward = 1;
-    
-    for(var i = 0; i < episodes; ++i)
-    {
-        //initialize s
-        var current = { x: randomFloorInterval(0,2), y: randomFloorInterval(0,2) };
-        for(var step = 0; step < steps; ++step)
-        {
-            var statePrime = current;
-            var max = getMax(statePrime);
-            
-            //take action a, observe r, s'
-            if (max == 0) 
-                statePrime.y--;
-            else if (max == 1) 
-                statePrime.x++;
-            else if (max == 2) 
-                statePrime.y++;
-            else if (max == 3) 
-                statePrime.x--;
-            
-            //print
-            var span = document.createElement('div');
-            span.innerText = (step+1).toString() + ": " + getCurrentStateText(current) + " " + getDirectionText(max);
-            actionList.appendChild(span);
-            
-            var qvalueCurrent = blocks[current.x][current.y].actions[max];
-            var maxQvaluePrime = blocks[current.x][current.y].actions[getMax(statePrime)];
-            var reward = blocks[statePrime.x][statePrime.y].reward;
-            
-            blocks[current.x][current.y].actions[max] = qvalueCurrent + theta * (reward + discount * maxQvaluePrime - qvalueCurrent);
-            
-            current = statePrime;
-        }
-    }
-    
-    function getValidDirection(state)
-    {
-        var directions = [];
-        if (state.y > 0) directions.push(0);
-        if (state.x < dimension - 1) directions.push(1);
-        if (state.y < dimension - 1) directions.push(2);
-        if (state.x > 0 > 0) directions.push(3);
-        return directions;
-    }
-    
-    function getMax(state)
-    {
-        var actions = blocks[state.x][state.y].actions;
-        var directions = getValidDirection(state);
-        var max = directions[0];
-        for(var i = 1; i < directions.length; ++i)
-        {
-            if(actions[i] > actions[max])
-                max = directions[i];
-        }
-        return max;
-    }
-    
-    
-    function getCurrentStateText(state)
-    {
-        return "Current state: (" + state.x + "," + state.y + ")";    
-    }
-    
-    function getDirectionText(direction)
-    {
-        if(direction == 0)
-            return "north";
-        else if(direction == 1)
-            return "east";
-        else if(direction == 2)
-            return "south";
-        else if (direction == 3)
-            return "west";
-        return "none";
-    }
-    
-    context.strokeStyle = 'black';
-    for(var row = 0, i = 0; row < canvas.width; row+=size, ++i)
-    {
-        var j = 0;
-        for(var col = 0, j = 0; col < canvas.width; col+=size, ++j)
-        {
-            //north-south
-            context.fillText(sliceQSA(i,j,0), row+(size/2)-16, col+20);
-            context.fillText(sliceQSA(i,j,2), row+(size/2)-16, col+size-10);
-            
-            //east-west
-            context.fillText(sliceQSA(i,j,1), row+(size/2)*1.55, col+(size/2));
-            context.fillText(sliceQSA(i,j,3), row+10, col+(size/2));
-            
-            //reward
-            context.fillText(blocks[i][j].reward.toString(), row+(size/2), col+(size/2));
-            
-            context.strokeRect(row, col, size, size);
-        }
-    }
-    
-    function sliceQSA(i, j, direction)
-    {
-        return blocks[i][j].actions[direction].toString().slice(0,6);
-    }
-    
-    function randomFloorInterval(min,max)
-    {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-    
-    function randomInterval(min,max)
-    {
-        return Math.random() * (max - min + 1) + min;
-    }
     
 }());
 
--- src/qlearning.js
+++ src/qlearning.js
@@ -1,51 +1,136 @@
-var Environment = function(dim) {  
+var Environment = function(dim, discount, stepsize) {  
     this.dimension = dim;
-    this.blocks = [];
+    this.blocks = initialize(this.dimension);
+    this.discount = discount;
+    this.stepsize = stepsize;
     
-    var Block = function(qvalue, reward) {
-        this.qvalue = qvalue;
-        this.reward = reward;
-    };
+    this.current = { x: randomFloorInterval(0,2), y: randomFloorInterval(0,2) };
     
-    var initializeDimensions = function (blocks, dimension)
+    function initialize(dimension)
     {
+        var blocks = [];
+        var randomInterval = function (min,max)
+        {
+            return Math.random() * (max - min + 1) + min;
+        };
+        
         for(var i = 0; i < dimension; ++i)
         {
             blocks.push([]);
             for(var j = 0; j < dimension; ++j)
             {
-                //0 = north, 1 = south, 2 = east, 3 = west
-                var directions = [0, 1, 2, 3];
-                var reward = 0;
-                blocks[i].push(new Block(directions, reward));
+                blocks[i].push({
+                    qsa: [randomInterval(0.45,0.66), randomInterval(0.35,0.7), randomInterval(0.4,0.58), randomInterval(0.35,0.7)],
+                    reward: 0
+                });
             }
         }
+        blocks[2][2].reward = 1;
+        return blocks;
     };
-    
-    initializeDimensions(this.blocks, this.dimension);
+        
+    function randomFloorInterval (min,max)
+    {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };     
 };
 
 Environment.prototype.draw = function(canvas, context) {
     var size = canvas.width / this.dimension;
     
+    context.clearRect(0, 0, canvas.width, canvas.height);
     context.strokeStyle = 'black';
     for(var row = 0, i = 0; row < canvas.width; row+=size, ++i)
     {
         var j = 0;
         for(var col = 0, j = 0; col < canvas.width; col+=size, ++j)
         {
-            for(var dir = 0; dir < 4; ++dir)
+            if(this.current.x === i && this.current.y === j)
             {
-                var q = this.blocks[i][j].qvalue[dir];
-                // context.fillText(qvdir, row / dir, col / dir, size, size);
+                context.fillStyle = 'blue';
+                context.fillRect(row, col, size, size);
             }
+            context.fillStyle = 'black';
+            //north-south
+            context.fillText(sliceQSA(this.blocks, i, j, 0), row+(size/2)-16, col+20);
+            context.fillText(sliceQSA(this.blocks, i, j, 2), row+(size/2)-16, col+size-10);
+            
+            //east-west
+            context.fillText(sliceQSA(this.blocks, i, j, 1), row+(size/2)*1.55, col+(size/2));
+            context.fillText(sliceQSA(this.blocks, i, j, 3), row+10, col+(size/2));
+            
+            //reward
+            context.fillText(this.blocks[i][j].reward.toString(), row+(size/2), col+(size/2));
+            
+            
             context.strokeRect(row, col, size, size);
         }
     }
+        
+    function sliceQSA(blocks, i, j, direction)
+    {
+        return blocks[i][j].qsa[direction].toString().slice(0,6);
+    };
 };
 
-Environment.prototype.train = function (agent) {
-    this.agent = agent;
+Environment.prototype.step = function (steps) {
+    
+    var current = this.current;
+    for(var step = 0; step < steps; ++step)
+    {
+        var future = { x: current.x, y: current.y };
+        var max = getMax(this.blocks[future.x][future.y], future, this.dimension);
+        
+        if (max == 0) 
+            future.y--;
+        else if (max == 1) 
+            future.x++;
+        else if (max == 2) 
+            future.y++;
+        else if (max == 3) 
+            future.x--;
+        
+        var qvalueCurrent = this.blocks[current.x][current.y].qsa[max];
+        var futureMax = getMax(this.blocks[future.x][future.y], future, this.dimension);
+        var maxQvalueFuture = this.blocks[future.x][future.y].qsa[futureMax];
+        var reward = this.blocks[future.x][future.y].reward;
+        
+        var qsaValue = qvalueCurrent + this.stepsize * (reward + this.discount * maxQvalueFuture - qvalueCurrent);
+        this.blocks[current.x][current.y].qsa[max] = qsaValue;
+        
+        this.current.x = future.x;
+        this.current.y = future.y;
+    }
+    
+    function getMax(actions, state, dimension)
+    {
+        var directions = getValidDirection(state, dimension);
+        var max = directions[0];
+        for(var i = 1; i < directions.length; ++i)
+        {
+            if(actions.qsa[directions[i]] > actions.qsa[max])
+            {
+                max = directions[i];
+            }
+        }
+        return max;
+    };
+    
+    
+    function getValidDirection(state, dimension)
+    {
+        var directions = [];
+        if (state.y > 0) directions.push(0);
+        if (state.x < dimension - 1) directions.push(1);
+        if (state.y < dimension - 1) directions.push(2);
+        if (state.x > 0 > 0) directions.push(3);
+        return directions;
+    };
+        
+    function randomFloorInterval(min,max)
+    {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
 };
 
 
