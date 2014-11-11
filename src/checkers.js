3d9c6e9b4a72e85fd6d4a872e1a9ad9239afb4d7
Kenneth Bastian <bastian.ai101@gmail.com>
2014-11-11 17:19:28 +0800

    move function

--- src/checkers.js
+++ src/checkers.js
@@ -1,5 +1,8 @@
-var Checkers = function(options) {
+var Checkers = function (options) {
+    options = options || {};
     this.dimension = 8;
+    this.blackAgent = options.blackAgent || {};
+    this.redAgent = options.redAgent || {};
 }
 
 Checkers.prototype = {
@@ -9,16 +12,33 @@ Checkers.prototype = {
         for (var i = 0; i < this.dimension; ++i) {
             this.blocks.push([]);
             for (var j = 0; j < this.dimension; ++j) {
-                this.blocks[i].push({
-                    state: "empty"
-                });
+                if (i % 2 != j % 2 && j <= 2) {
+                    this.blocks[i].push({
+                        state: "red"
+                    });
+                }
+                else if (i % 2 != j % 2 && j >= 5) {
+                    this.blocks[i].push({
+                        state: "black"
+                    });
+                }
+                else if (i % 2 == j % 2) {
+                    this.blocks[i].push({
+                        state: "invalid"
+                    });
+                }
+                else {
+                    this.blocks[i].push({
+                        state: "empty"
+                    });
+                }
             }
         }
     },
 
     draw: function (canvas, context) {
         var size = canvas.width / this.dimension;
-
+        var blks = this.blocks;
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         for (var row = 0, i = 0; row < canvas.width; row += size, ++i) {
@@ -28,12 +48,54 @@ Checkers.prototype = {
             }
         }
 
+        var redPiece = new Image();
+        redPiece.onload = function () {
+            for (var row = 0, i = 0; row < canvas.width; row += size, ++i) {
+                for (var col = 0, j = 0; col < canvas.width; col += size, ++j) {
+                    if (blks[i][j].state === 'red') {
+                        context.drawImage(redPiece, row + 11, col + 11, size * 0.7, size * 0.7);
+                    }
+                }
+            }
+        }
+        redPiece.src = "contents/red.gif";
+
         var blackPiece = new Image();
         blackPiece.onload = function () {
-            context.drawImage(blackPiece, 87, 14, 50, 50);
-            context.drawImage(blackPiece, 87, 164, 50, 50);
-            context.drawImage(blackPiece, 13, 89, 50, 50);
-        };
+            for (var row = 0, i = 0; row < canvas.width; row += size, ++i) {
+                for (var col = 0, j = 0; col < canvas.width; col += size, ++j) {
+                    if (blks[i][j].state === 'black') {
+                        context.drawImage(blackPiece, row + 11, col + 11, size * 0.7, size * 0.7)
+                    }
+                }
+            }
+        }
         blackPiece.src = "contents/black.gif";
+    },
+
+    move: function (color, fromX, fromY, destX, destY) {
+        var isValid = isMoveValid(this.blocks, {
+            color: color,
+            fromX: fromX,
+            fromY: fromY,
+            destX: destX,
+            destY: destY
+        });
+        if (isValid) {
+            this.blocks[fromX][fromY].state = "empty";
+            this.blocks[destX][destY].state = color;
+            return true;
+        }
+        return false;
     }
+}
+
+function isMoveValid(blocks, options)
+{
+    var destColor = blocks[options.destX][options.destY].state;
+    var fromColor = blocks[options.fromX][options.fromY].state;
+    if (destColor === 'black' || destColor === 'red' || destColor === 'invalid')
+        return false;
+    if (fromColor != options.color) return false;
+    return true;
 }
--- src/play.js
+++ src/play.js
@@ -1,7 +1,17 @@
-(function(){
-    var checkers = new Checkers();
+(function () {
+    var checkers = new Checkers({});
     var canvas = document.querySelector("#canvas");
     var context = canvas.getContext('2d');
     checkers.initialize();
     checkers.draw(canvas, context);
-}());
+
+    checkers.move("red", 1, 2, 0, 3);
+    checkers.draw(canvas, context);
+
+    checkers.move("black", 0, 5, 0, 3);
+    checkers.draw(canvas, context);
+
+    checkers.move("black", 0, 5, 1, 4);
+    checkers.draw(canvas, context);
+
+} ());
