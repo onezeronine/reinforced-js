f080e60730b0f6913d355671e87d5684678730d0
onezeronine <bastian.kenneth.g@gmail.com>
2014-11-15 19:27:00 +0800

    onclick move method

--- src/play.js
+++ src/play.js
@@ -1,11 +1,11 @@
 (function () {
     var canvas = document.querySelector("#canvas"),
         context = canvas.getContext('2d'),
-        selection = {
+        track = {
             x: -1,
             y: -1,
             color: "none",
-            state: "none"
+            selected: false
         },
         checkers = new Checkers({
             canvas: canvas,
@@ -17,14 +17,34 @@
     checkers.draw();
 
     var appendTo = function (index) {
-        var text = index.color + ": (" + index.fromX + "," + index.fromY + ")";
+        var text = track.color + ": (" + index.fromX + "," + index.fromY + ") to (" +
+                    track.x + "," + track.y + ")";
         history.append($("<div>").text(text));
     };
 
     var canvasOnClick = function (e) {
         var coordinates = canvas.relMouseCoords(e);
         var index = checkers.getIndexFromCoordinates(coordinates.x, coordinates.y);
-        appendTo(index);
+
+        if (track.selected) {
+            appendTo(index);
+
+            checkers.move(track.color, track.x, track.y, index.fromX, index.fromY);
+            checkers.draw();
+
+            track.x = -1;
+            track.y = -1;
+            track.color = "none";
+            track.selected = false;
+        }
+        else if (!track.selected) {
+            track.x = index.fromX;
+            track.y = index.fromY;
+            track.color = index.color;
+            track.selected = true;
+        }
+
+
     };
     canvas.addEventListener('click', canvasOnClick);
 
